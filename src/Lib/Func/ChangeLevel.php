03fd933b5d36f42326ef1e1a4d7d3ec8483851a7
Masterflash <sxq@yandex.ru>
2019-02-18 19:35:42 +0300

    Update ChangeLevel.php

--- src/Lib/Func/ChangeLevel.php
+++ src/Lib/Func/ChangeLevel.php
@@ -10,20 +10,26 @@ class ChangeLevel
 
 
 
-public function __invoke ($obj,$infa,$struct0,$struct2,$tab_name,$const,$row_item,$a,$b,$action)
+public function __invoke ($obj,$infa,$struct0)
 {
-    $infa=(int)$infa;
-    if (empty($infa)){
-        return $infa;
-    }
-    $r=simba::queryOneRecord("select level from catalog_category where id={$infa}");
-    $r["level"]++;
-    simba::query("update catalog_category set level=1+".$r["level"]." where id={$b}");
-    return $infa;
+    
+    
+    $this->tovtree1(0,0);
+
 }
 
 
 
+protected function tovtree1($subid,$lev)
+{
+    //получить список подразделов
+    $razd=simba::queryAllRecords("SELECT id,subid,level FROM  catalog_category  where subid=".$subid); 
+    $count=simba::numRows();
+    for ($i=0; $i<$count;$i++){
+        $this->tovtree1 ($razd['id'][$i],$lev+1);
+    }
+    simba::query("update catalog_category set level=$lev  where subid=".$subid); 
+}
 
     
 }
